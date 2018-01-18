d8d837fb918bd371c998c653477e56de9fd51d40
Anusree Sajeevan <anusreesajeevan111@gmail.com>
2018-01-18 13:13:02 +0530

    Initial commit  - Create project with C++ support  - Create Oscillator and add to cmakelist

---
+++ app/src/main/cpp/Oscillator.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Design on 18-01-2018.
+//
+
+#include "Oscillator.h"
---
+++ app/src/main/cpp/Oscillator.h
@@ -0,0 +1,14 @@
+//
+// Created by Design on 18-01-2018.
+//
+
+#ifndef WAVEMAKER_OSCILLATOR_H
+#define WAVEMAKER_OSCILLATOR_H
+
+
+class Oscillator {
+
+};
+
+
+#endif //WAVEMAKER_OSCILLATOR_H
