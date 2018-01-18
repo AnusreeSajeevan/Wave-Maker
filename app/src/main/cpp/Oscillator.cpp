8e3d98a2c83835cc29dd40cecf6421ec92f5bcdf
Anusree Sajeevan <anusreesajeevan111@gmail.com>
2018-01-18 13:31:23 +0530

    Build the oscillator

--- app/src/main/cpp/Oscillator.cpp
+++ app/src/main/cpp/Oscillator.cpp
@@ -3,3 +3,53 @@
 //
 
 #include "Oscillator.h"
+#include <math.h>
+
+#define TWO_PI (3.14159 * 2)
+#define AMPLITUDE 0.3
+#define FREQUENCY 440.0
+
+/**
+ * method allows us to set the desired sample rate for our audio data
+ * Based on sampleRate and FREQUENCY it calculates the value of phaseIncrement_, which is used in render.
+ * If you want to change the pitch of the sine wave just update FREQUENCY with a new value.
+ * @param sampleRate
+ */
+void Oscillator::setSampleRate(int32_t sampleRate) {
+    phaseIncrement_ = (TWO_PI * FREQUENCY) / (double) sampleRate;
+}
+
+/**
+ * This is used in render to determine whether to output the sine wave or silence.
+ * @param isWaveOn is false we just output zeros (silence).
+ */
+void Oscillator::setWaveOn(bool isWaveOn) {
+    isWaveOn_.store(isWaveOn);
+}
+
+/**
+ *
+ * @param audioData
+ * @param numFrames is the number of audio frames which we must render.
+ */
+void Oscillator::render(float *audioData, int32_t numFrames) {
+
+    if (!isWaveOn_.load()) phase_ = 0;
+
+    for (int i = 0; i < numFrames; i++) {
+
+        if (isWaveOn_.load()) {
+
+            // Calculates the next sample value for the sine wave.
+            audioData[i] = (float) (sin(phase_) * AMPLITUDE);
+
+            // Increments the phase, handling wrap around.
+            phase_ += phaseIncrement_;
+            if (phase_ > TWO_PI) phase_ -= TWO_PI;
+
+        } else {
+            // Outputs silence by setting sample value to zero.
+            audioData[i] = 0;
+        }
+    }
+}
--- app/src/main/cpp/Oscillator.h
+++ app/src/main/cpp/Oscillator.h
@@ -5,9 +5,24 @@
 #ifndef WAVEMAKER_OSCILLATOR_H
 #define WAVEMAKER_OSCILLATOR_H
 
+#include <stdint.h>
+#include <atomic>
 
 class Oscillator {
+public:
+    void setWaveOn(bool isWaveOn);
+    void setSampleRate(int32_t sampleRate);
+    void render(float *audio, int32_t numFrames);
 
+private:
+//    define this as atomis bool cause it will be accessing from multiple threads
+    std::atomic<bool> isWaveOn_{false};
+
+    /**
+     * phase_ stores the current wave phase, and it is incremented by phaseIncrement_ after each sample is generated.
+     */
+    double phase_ = 0.0;
+    double phaseIncrement_ = 0.0;
 };
 
 
